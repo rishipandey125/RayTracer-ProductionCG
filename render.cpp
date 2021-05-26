2540c0983178e521a187fc820a07d47f2cff825f
Rishi <rishipandey125@gmail.com>
2021-05-26 00:02:46 -0400

    removed mesh related files

--- render.cpp
+++ render.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
-#include "tinyobjloader-master/tiny_obj_loader.h"
 #include "vec.cpp"
 #include "ray.cpp"
 #include "aabb.cpp"
@@ -10,97 +8,12 @@
 #include "bvh.cpp"
 #include "camera.cpp"
 #include "sphere.cpp"
-#include "triangle.cpp"
 #include "plane.cpp"
 #include <vector>
 
 //Render File
 //Constant Epsilon
 const float EPSILON = 0.0001;
-
-/*
-OBJ Loading Function:
-@return hittables: mesh of triangles in the form of a hittables
-*/
-hittables load_obj_file(std::string inputfile) {
-  tinyobj::ObjReaderConfig reader_config;
-
-  tinyobj::ObjReader reader;
-
-  if (!reader.ParseFromFile(inputfile, reader_config)) {
-    if (!reader.Error().empty()) {
-        std::cerr << "TinyObjReader: " << reader.Error();
-    }
-    exit(1);
-  }
-
-  if (!reader.Warning().empty()) {
-    std::cout << "TinyObjReader: " << reader.Warning();
-  }
-
-  auto& attrib = reader.GetAttrib();
-  auto& shapes = reader.GetShapes();
-  // auto& materials = reader.GetMaterials();
-  hittables mesh;
-  std::vector <vec> vertices = {};
-  // Loop over shapes
-  for (size_t s = 0; s < shapes.size(); s++) {
-    // Loop over faces(polygon)
-    size_t index_offset = 0;
-    //Loop Over Each Triangle
-    for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-      int fv = shapes[s].mesh.num_face_vertices[f];
-      // Loop over vertices in the face.
-      for (size_t v = 0; v < fv; v++) {
-        // access to vertex
-        tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-        tinyobj::real_t vx = attrib.vertices[3*idx.vertex_index+0];
-        tinyobj::real_t vy = attrib.vertices[3*idx.vertex_index+1];
-        tinyobj::real_t vz = attrib.vertices[3*idx.vertex_index+2];
-        vertices.push_back(vec(vx,vy,vz));
-      }
-      index_offset += fv;
-      // per-face material
-      shapes[s].mesh.material_ids[f];
-    }
-  }
-
-  std::vector <vec> vertex_normals = {};
-  for (int i = 0; i < attrib.vertices.size() / 3; i++) {
-      vertex_normals.push_back(vec());
-  }
-
-  int num_triangles = vertices.size()/3;
-  for (int i = 0; i < num_triangles; i++) {
-    vec vertex1 = vertices[i*3];
-    vec vertex2 = vertices[(i*3)+1];
-    vec vertex3 = vertices[(i*3)+2];
-    vec edge1 = vertex2 - vertex1;
-    vec edge2 = vertex3 - vertex1;
-    vec face_normal = edge1.cross(edge2);
-
-    for (int x = 0; x < 3; x++) {
-      int idx = shapes[0].mesh.indices[3*i+x].vertex_index;
-      vertex_normals[idx] = vertex_normals[idx] + face_normal;
-    }
-  }
-  //create the mesh
-  for (int i = 0; i < num_triangles; i++) {
-    vec vertex1 = vertices[i*3];
-    vec vertex2 = vertices[(i*3)+1];
-    vec vertex3 = vertices[(i*3)+2];
-    int idx1 = shapes[0].mesh.indices[3*i].vertex_index;
-    int idx2 = shapes[0].mesh.indices[3*i+1].vertex_index;
-    int idx3 = shapes[0].mesh.indices[3*i+2].vertex_index;
-    vec v_n1 = vertex_normals[idx1];
-    vec v_n2 = vertex_normals[idx2];
-    vec v_n3 = vertex_normals[idx3];
-    v_n1.unit(); v_n2.unit(); v_n3.unit();
-    mesh.add(new triangle(vertex1,vertex2,vertex3,v_n1,v_n2,v_n3,new diffuse(color(0.578,.439,.856))));
-  }
-
-  return mesh;
-}
 
 /*
 Trace function, traces rays through the scene based on the trace depth, and materials of the hit objects.
@@ -157,8 +70,8 @@ void render_frame() {
 
   int image_height = (int)(image_width/cam.aspect_ratio);
   // Creating Scene Geometry
-  hittables scene_geometry;
   //floor
+  hittables scene_geometry;
   scene_geometry.add(new sphere(point(0,-100.5,-1),100,new diffuse(color(.75,.75,.75))));
   //top light source
   scene_geometry.add(new plane(point(-1,1.5,-2),point(1,1.5,-2),point(-1,1.5,-3),point(1,1.5,-3),new diffuse_light(color(1,1,1))));
